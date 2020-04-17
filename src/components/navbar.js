e835b6cd7bb1a22857a6796b65464e5fa7bf53b5
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-04-17 22:02:10 +0600

    Simplify

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,21 +2,19 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../logo.svg'
 
-const Navbar = () => {
-    return (
-        <nav className="uk-navbar-container" data-uk-navbar="">
-            <div className="uk-navbar-left">
-                <a href="/" className="uk-navbar-item uk-logo">
-                    <img src={logo} alt="" style={{height: 20}} data-uk-img=""/>
-                </a>
-                <ul className="uk-navbar-nav">
-                    <li><Link to="/">Standings</Link></li>
-                    <li><Link to="/drivers">Drivers</Link></li>
-                    <li><Link to="/teams">Teams</Link></li>
-                </ul>
-            </div>
-        </nav>
-    );
-};
+const Navbar = () => (
+    <nav className="uk-navbar-container" data-uk-navbar="">
+        <div className="uk-navbar-left">
+            <a href="/" className="uk-navbar-item uk-logo">
+                <img src={logo} alt="" style={{height: 20}} data-uk-img=""/>
+            </a>
+            <ul className="uk-navbar-nav">
+                <li><Link to="/">Standings</Link></li>
+                <li><Link to="/drivers">Drivers</Link></li>
+                <li><Link to="/teams">Teams</Link></li>
+            </ul>
+        </div>
+    </nav>
+);
 
 export default Navbar;

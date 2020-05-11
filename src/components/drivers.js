3bbbb5c11581a9ca4a13d587ad1719a00aff31bb
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-05-11 21:55:58 +0600

    Filter drivers

--- src/components/drivers.js
+++ src/components/drivers.js
@@ -4,24 +4,29 @@ import LinkDriver from './link/driver';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faEllipsisH} from '@fortawesome/free-solid-svg-icons';
 import _ from 'lodash';
+import styled from 'styled-components';
+
+const NameSpan = styled.span`
+    &.hl {
+        background-color: yellow;
+    }
+`;
 
 function List({letter, drivers}) {
     return (
         <dl className="uk-margin-remove">
             {letter && (
-                <dt>
-                    <span className="uk-text-large">{letter}</span>
-                    {' '}
-                    <small className="uk-text-muted">({drivers.length})</small>
-                </dt>
+                <dt className="uk-text-large">{letter}</dt>
             )}
             {drivers.map(driver => {
-                const {driverId, familyName, givenName, hidden} = driver;
+                const {driverId, familyName, givenName, visible} = driver;
 
                 return (
-                    <dd key={driverId} className={hidden ? 'uk-hidden' : ''}>
+                    <dd key={driverId} className={visible ? '' : 'uk-hidden'}>
                         <LinkDriver driver={driver}>
-                            {familyName}, {givenName}
+                            <NameSpan>{familyName}</NameSpan>,
+                            {' '}
+                            <NameSpan>{givenName}</NameSpan>
                         </LinkDriver>
                     </dd>
                 );
@@ -35,7 +40,7 @@ export default function Drivers() {
         [busy, setBusy] = useState(true),
         [data, setData] = useState({}),
         [modalContent, setModalContent] = useState([]),
-        [filterText, setFilterText] = useState('');
+        [filter, setFilter] = useState('');
 
     useEffect(() => {
         const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
@@ -55,13 +60,10 @@ export default function Drivers() {
 
                 arr.sort();
 
-                data$[letter] = {
-                    hidden: false,
-                    drivers: arr.map((driver, i) => {
-                        driver.hidden = i > 9;
-                        return driver;
-                    })
-                };
+                data$[letter] = arr.map((driver, i) => {
+                    driver.visible = i < 9;
+                    return driver;
+                });
             });
 
             if (isMounted) {
@@ -75,40 +77,76 @@ export default function Drivers() {
         };
     }, []);
 
+    function toggleDrivers(drivers, visible = true) {
+        drivers.forEach(driver => {
+            driver.visible = visible;
+        });
+    }
+
     function showAllDrivers(letter) {
-        const {drivers} = _.cloneDeep(data[letter]);
+        const drivers = _.cloneDeep(data[letter]);
+
+        toggleDrivers(drivers);
 
         setModalContent([
             letter,
-            drivers.map(driver => {
-                driver.hidden = false;
-                return driver;
-            })
+            drivers
         ]);
     }
 
     function handleSearch(e) {
-        const filter = e.target.value;
-        // todo
-        setFilterText(filter);
+        const filter$ = (e.target.value || '').trim().toLowerCase();
+
+        setFilter(filter$);
+
+        setData(prevState => {
+            const state = {};
+
+            Object.keys(prevState).forEach(letter => {
+                const drivers = _.cloneDeep(prevState[letter]);
+
+                toggleDrivers(drivers, false);
+
+                drivers.forEach((driver, i) => {
+                    const
+                        {givenName, familyName} = driver,
+                        str1 = givenName.toLowerCase().includes(filter$),
+                        str2 = familyName.toLowerCase().includes(filter$);
+                        // , regexp = new RegExp(filter$, 'ig');
+
+                    driver.visible = filter$
+                        ? str1 || str2
+                        : i < 9;
+
+                    // str1 && (driver.givenName = givenName.replace(regexp, '+'));
+                    // str2 && (driver.familyName = familyName.replace(regexp, '-'));
+                });
+
+                state[letter] = drivers;
+            });
+
+            return state;
+        });
     }
+
+    let visibleCards = 0;
 
     return (
         <div className="uk-padding-small">
             {busy ? <span data-uk-spinner=""/> : (
                 <>
                     <div data-uk-grid="" className="uk-grid-small uk-flex uk-flex-middle">
-                        <div className="uk-width-1-4">
+                        <div className="uk-width-3-5">
                             <h1 className="uk-text-uppercase">Drivers</h1>
                         </div>
-                        <div className="uk-width-1-2">
+                        <div className="uk-width-expand">
                             <form
                                 onSubmit={(e) => {e.preventDefault()}}
                                 className="uk-search uk-search-default uk-width-1-1">
                                 <span data-uk-search-icon=""/>
                                 <input
                                     onChange={(e) => handleSearch(e)}
-                                    value={filterText}
+                                    value={filter}
                                     type="search"
                                     className="uk-search-input"
                                     placeholder="Search..."/>
@@ -118,21 +156,25 @@ export default function Drivers() {
                     <div
                         data-uk-grid=""
                         data-uk-height-match="target: > div > .uk-card; row: false"
-                        className="uk-grid-small">
+                        className="uk-grid-small uk-grid-match">
                         {Object.keys(data).map(letter => {
-                            const {hidden, drivers} = data[letter];
+                            const
+                                drivers = data[letter],
+                                driversCount = drivers.length,
+                                visibleDriversCount = drivers.filter(driver => driver.visible).length,
+                                showCard = visibleDriversCount > 0;
 
-                            return drivers.length > 0 ? (
-                                <div
-                                    key={`drivers-${letter}`}
-                                    className={`uk-width-1-5${hidden ? ' uk-hidden' : ''}`}>
+                            visibleCards += Number(showCard);
+
+                            return showCard > 0 ? (
+                                <div key={letter} className="uk-width-1-5">
                                     <div className="uk-card uk-card-default uk-card-body">
                                         <List letter={letter} drivers={drivers}/>
-                                        {drivers.length > 10 && (
+                                        {driversCount > visibleDriversCount && (
                                             <button
                                                 onClick={() => showAllDrivers(letter)}
                                                 data-uk-toggle="target: #modal"
-                                                title="Show all"
+                                                title={`Show all ${driversCount} drivers`}
                                                 className="uk-button uk-button-link">
                                                 <FontAwesomeIcon icon={faEllipsisH}/>
                                             </button>
@@ -142,6 +184,12 @@ export default function Drivers() {
                             ) : null;
                         })}
                     </div>
+
+                    {visibleCards === 0 && (
+                        <div className="uk-text-center uk-margin-medium">
+                            No matching results found for <b>&laquo;{filter}&raquo;</b>
+                        </div>
+                    )}
 
                     <div id="modal" data-uk-modal="">
                         <div data-uk-overflow-auto="" className="uk-modal-dialog">

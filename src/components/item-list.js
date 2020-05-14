4f907034cef675c4141952613a98120200c644d0
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-05-14 15:25:13 +0600

    Show all drivers, but under overflow

--- src/components/item-list.js
+++ src/components/item-list.js
@@ -1,57 +1,12 @@
 import React, {Fragment, useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faEllipsisH} from '@fortawesome/free-solid-svg-icons';
-import _ from 'lodash';
-
-function List({letter, items, props, onClick, filter = ''}) {
-    const
-        regexp = new RegExp(`(${filter})`, 'ig'),
-        replace = '<span style="background-color: #FFFF33">$1</span>';
-
-    return (
-        <dl className="uk-margin-remove">
-            {letter && <dt className="uk-text-large">{letter}</dt>}
-
-            {items.map((item, i) => {
-                const {visible} = item;
-                const obj = {...item};
-
-                filter && props.forEach(prop => {
-                    const {[prop]: str} = obj;
-
-                    obj[prop] = regexp.test(str)
-                        ? str.replace(regexp, replace)
-                        : str;
-                });
-
-                return (
-                    <dd key={`item-${i}`} className={visible ? '' : 'uk-hidden'}>
-                        <a href="/" onClick={(e) => {
-                            e.preventDefault();
-                            onClick(item);
-                        }}>
-                            {props.map((prop, j) => {
-                                return (
-                                    <Fragment key={j}>
-                                        <span dangerouslySetInnerHTML={{__html: obj[prop]}}/>
-                                        {j === props.length - 1 ? '' : ', '}
-                                    </Fragment>
-                                );
-                            })}
-                        </a>
-                    </dd>
-                );
-            })}
-        </dl>
-    );
-}
 
 export default function ItemList({heading, items, props, onClick}) {
     const
-        [data, setData] = useState({}),
-        [modalData, setModalData] = useState([]),
-        [filter, setFilter] = useState('');
+        [data, setData] = useState(null),
+        [filter, setFilter] = useState(''),
+        regexp = new RegExp(`(${filter})`, 'ig'),
+        replace = '<span style="background-color: #FFFF33">$1</span>';
 
     useEffect(() => {
         const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
@@ -67,37 +22,14 @@ export default function ItemList({heading, items, props, onClick}) {
 
                 array.sort();
 
-                state[letter] = array.map((item, i) => {
-                    item.visible = i < 9;
-                    return item;
-                });
+                array.forEach(item => item.visible = true);
+
+                state[letter] = array;
             });
 
             return state;
         });
-
-        return () => {
-            setData({});
-            setModalData(null);
-        };
     }, [items, props]);
-
-    function toggleItems(array, visible = true) {
-        array.forEach(item => {
-            item.visible = visible;
-        });
-    }
-
-    function showAllItems(letter) {
-        const array = _.cloneDeep(data[letter]);
-
-        toggleItems(array);
-
-        setModalData([
-            letter,
-            array
-        ]);
-    }
 
     function handleSearch(e) {
         const f = (e.target.value || '').trim().toLowerCase();
@@ -108,11 +40,9 @@ export default function ItemList({heading, items, props, onClick}) {
             const state = {};
 
             Object.keys(prevState).forEach(letter => {
-                const array = _.cloneDeep(prevState[letter]);
+                const array = prevState[letter];
 
-                toggleItems(array, false);
-
-                array.forEach((item, i) => {
+                array.forEach(item => {
                     let found = 0;
 
                     props.forEach(prop => {
@@ -120,7 +50,7 @@ export default function ItemList({heading, items, props, onClick}) {
                         found += Number(str.toLowerCase().includes(f));
                     });
 
-                    item.visible = f ? found > 0 : i < 9;
+                    item.visible = f ? found > 0 : true;
                 });
 
                 state[letter] = array;
@@ -132,7 +62,7 @@ export default function ItemList({heading, items, props, onClick}) {
 
     let visibleCards = 0;
 
-    return (
+    return data ? (
         <>
             <div data-uk-grid="" className="uk-grid-small uk-flex uk-flex-middle">
                 <div className="uk-width-3-5">
@@ -159,7 +89,6 @@ export default function ItemList({heading, items, props, onClick}) {
                 {Object.keys(data).map(letter => {
                     const
                         array = data[letter],
-                        itemsCount = array.length,
                         visibleItemsCount = array.filter(item => item.visible).length,
                         showCard = visibleItemsCount > 0;
 
@@ -168,17 +97,43 @@ export default function ItemList({heading, items, props, onClick}) {
                     return showCard > 0 ? (
                         <div key={letter} className="uk-width-1-5">
                             <div className="uk-card uk-card-default uk-card-body">
-                                <List {...{letter, items: array, props, onClick, filter}}/>
+                                <dl className="uk-margin-remove uk-overflow-auto" style={{maxHeight: 300}}>
+                                    <dt className="uk-text-large">{letter}</dt>
 
-                                {itemsCount > visibleItemsCount && (
-                                    <button
-                                        onClick={() => showAllItems(letter)}
-                                        data-uk-toggle="target: #modal"
-                                        title={`Show all ${itemsCount}`}
-                                        className="uk-button uk-button-link">
-                                        <FontAwesomeIcon icon={faEllipsisH}/>
-                                    </button>
-                                )}
+                                    {array.map((item, i) => {
+                                        const {visible} = item;
+                                        const item$ = {...item};
+
+                                        filter && props.forEach(prop => {
+                                            const {[prop]: str} = item$;
+
+                                            // just replace string value(s)
+                                            // equivalent to:
+                                            // item.prop = test ? item.prop.replace(...) : item.prop
+                                            item$[prop] = regexp.test(str)
+                                                ? str.replace(regexp, replace)
+                                                : str;
+                                        });
+
+                                        return (
+                                            <dd key={`item-${i}`} className={visible ? '' : 'uk-hidden'}>
+                                                <a href="/" onClick={(e) => {
+                                                    e.preventDefault();
+                                                    onClick(item);
+                                                }}>
+                                                    {props.map((prop, j) => {
+                                                        return (
+                                                            <Fragment key={j}>
+                                                                <span dangerouslySetInnerHTML={{__html: item$[prop]}}/>
+                                                                {j === props.length - 1 ? '' : ', '}
+                                                            </Fragment>
+                                                        );
+                                                    })}
+                                                </a>
+                                            </dd>
+                                        );
+                                    })}
+                                </dl>
                             </div>
                         </div>
                     ) : null;
@@ -190,29 +145,8 @@ export default function ItemList({heading, items, props, onClick}) {
                     No matching results found for <b>&laquo;{filter}&raquo;</b>
                 </div>
             ) : null}
-
-            <div id="modal" data-uk-modal="">
-                <div data-uk-overflow-auto="" className="uk-modal-dialog">
-                    {(() => {
-                        const [letter, array = []] = modalData;
-
-                        return (
-                            <>
-                                <div className="uk-modal-header">
-                                    <h2 className="uk-modal-title">{letter}</h2>
-                                </div>
-                                <div className="uk-modal-body">
-                                    <div className="uk-column-1-3">
-                                        <List {...{items: array, props, onClick}}/>
-                                    </div>
-                                </div>
-                            </>
-                        );
-                    })()}
-                </div>
-            </div>
         </>
-    );
+    ) : null;
 }
 
 ItemList.propTypes = {

f4cb8d4a029e65b6705defff8521e015badf06c7
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-06-10 16:55:40 +0600

    2013-2015 seasons

--- public/scripts/season.php
+++ public/scripts/season.php
@@ -5,65 +5,69 @@ require 'request.php';
 
 ['y' => $year] = getopt('y:');
 
-fwrite(STDOUT, "--- {$year} ---\n");
+$years = is_array($year) ? $year : [$year];
 
-$season = request($year.'/results/1');
-$races = json_decode($season)->MRData->RaceTable->Races;
-$baseDir = '../api/f1/'.$year;
+foreach ($years as $year) {
+    fwrite(STDOUT, "--- {$year} ---\n");
 
-@mkdir($baseDir.'/results', 0755, true);
+    $season = request($year.'/results/1');
+    $races = json_decode($season)->MRData->RaceTable->Races;
+    $baseDir = '../api/f1/'.$year;
 
-if (!file_exists($baseDir.'/results/1.json')) {
-    fwrite(STDOUT, "- results\n");
-    file_put_contents($baseDir.'/results/1.json', $season);
-}
+    @mkdir($baseDir.'/results', 0755, true);
 
-if (!file_exists($baseDir.'/driverStandings.json')) {
-    fwrite(STDOUT, "- driver standings\n");
-    file_put_contents($baseDir.'/driverStandings.json', request($year.'/driverStandings'));
-}
-
-if (!file_exists($baseDir.'/constructorStandings.json')) {
-    fwrite(STDOUT, "- constructor standings\n");
-    file_put_contents($baseDir.'/constructorStandings.json', request($year.'/constructorStandings'));
-}
-
-foreach ($races as $race) {
-    $round = $race->round;
-    $roundDir = $baseDir.'/'.$round;
-
-    fwrite(STDOUT, "--- round {$round} ---\n");
-
-    @mkdir($roundDir, 0755, true);
-
-    foreach (['results', 'qualifying', 'pitstops'] as $path) {
-        $file = $roundDir.'/'.$path.'.json';
-
-        if (!file_exists($file)) {
-            fwrite(STDOUT, "- {$path}\n");
-            file_put_contents($file, request($year.'/'.$round.'/'.$path));
-        }
+    if (!file_exists($baseDir.'/results/1.json')) {
+        fwrite(STDOUT, "- results\n");
+        file_put_contents($baseDir.'/results/1.json', $season);
     }
 
-    $lapsFile = $roundDir.'/laps.json';
+    if (!file_exists($baseDir.'/driverStandings.json')) {
+        fwrite(STDOUT, "- driver standings\n");
+        file_put_contents($baseDir.'/driverStandings.json', request($year.'/driverStandings'));
+    }
 
-    if (!file_exists($lapsFile)) {
-        fwrite(STDOUT, "--- laps ---\n");
-        fwrite(STDOUT, "- lap 1\n");
+    if (!file_exists($baseDir.'/constructorStandings.json')) {
+        fwrite(STDOUT, "- constructor standings\n");
+        file_put_contents($baseDir.'/constructorStandings.json', request($year.'/constructorStandings'));
+    }
 
-        $laps = json_decode(request($year.'/'.$round.'/laps/1'));
+    foreach ($races as $race) {
+        $round = $race->round;
+        $roundDir = $baseDir.'/'.$round;
 
-        for ($i = 2; $i <= $race->Results[0]->laps; $i++) {
-            fwrite(STDOUT, "- lap {$i}\n");
+        fwrite(STDOUT, "--- round {$round} ---\n");
 
-            $response = json_decode(request($year.'/'.$round.'/laps/'.$i));
+        @mkdir($roundDir, 0755, true);
 
-            array_push(
-                $laps->MRData->RaceTable->Races[0]->Laps,
-                $response->MRData->RaceTable->Races[0]->Laps[0]
-            );
+        foreach (['results', 'qualifying', 'pitstops'] as $path) {
+            $file = $roundDir.'/'.$path.'.json';
+
+            if (!file_exists($file)) {
+                fwrite(STDOUT, "- {$path}\n");
+                file_put_contents($file, request($year.'/'.$round.'/'.$path));
+            }
         }
 
-        file_put_contents($lapsFile, json_encode($laps));
+        $lapsFile = $roundDir.'/laps.json';
+
+        if (!file_exists($lapsFile)) {
+            fwrite(STDOUT, "--- laps ---\n");
+            fwrite(STDOUT, "- lap 1\n");
+
+            $laps = json_decode(request($year.'/'.$round.'/laps/1'));
+
+            for ($i = 2; $i <= $race->Results[0]->laps; $i++) {
+                fwrite(STDOUT, "- lap {$i}\n");
+
+                $response = json_decode(request($year.'/'.$round.'/laps/'.$i));
+
+                array_push(
+                    $laps->MRData->RaceTable->Races[0]->Laps,
+                    $response->MRData->RaceTable->Races[0]->Laps[0]
+                );
+            }
+
+            file_put_contents($lapsFile, json_encode($laps));
+        }
     }
 }

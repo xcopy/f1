f1328e145b1e9fa0a3f446342771e5f7810b4629
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-06-11 15:51:52 +0600

    Seasons 2010-12

--- src/components/season/layout.js
+++ src/components/season/layout.js
@@ -5,7 +5,7 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faList, faUser, faCar} from '@fortawesome/free-solid-svg-icons';
 import _ from 'lodash';
 
-export const minYear = 2013;
+export const minYear = 2010;
 export const currentYear = new Date().getFullYear();
 
 export default class SeasonLayout extends Component {

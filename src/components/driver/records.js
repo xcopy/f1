c11b31430ec493c70c9f8a771824c6147a7db808
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-06-18 15:54:35 +0600

    Records component refactored

--- src/components/driver/records.js
+++ src/components/driver/records.js
@@ -5,74 +5,97 @@ import {faInfoCircle} from '@fortawesome/free-solid-svg-icons';
 import Spinner from '../spinner';
 import Card from '../card';
 
+const Leader = ({label, primary, secondary}) => {
+    return (
+        <div className="uk-grid-small" data-uk-grid="">
+            <div className="uk-width-expand" data-uk-leader="">{label}</div>
+            <div>
+                <span className="uk-text-bold">{primary}</span>
+                {' '}
+                {secondary && <span className="uk-text-muted">({secondary})</span>}
+            </div>
+        </div>
+    );
+};
+
 export default function DriverRecords({standings, races}) {
     const
         {busy: loadingStandings, data: Standings} = standings,
         {busy: loadingRaces, data: Races} = races;
 
-    function getTitlesCount() {
-        return Standings.filter(standing => {
-            const {DriverStandings, ConstructorStandings} = standing;
+    function round(num, exp = 2) {
+        const pow = Math.pow(10, exp);
+        return Math.round((num + Number.EPSILON) * pow) / pow;
+    }
+
+    function getTitles() {
+        return Standings.filter(({DriverStandings, ConstructorStandings}) => {
             const [{position}] = (DriverStandings || ConstructorStandings);
             return parseInt(position) === 1;
         }).length;
     }
 
-    function getPolePositionsCount() {
-        return Races.filter(race => {
-            const {Results: [{grid}]} = race;
-            return parseInt(grid) === 1;
-        }).length;
+    function getPoles() {
+        return Races.filter(({Results: [{grid}]}) => parseInt(grid) === 1).length;
     }
 
-    function getTotalCountOf(prop) {
-        return Standings.map(standing => {
-            const {DriverStandings, ConstructorStandings} = standing;
-            const [obj] = (DriverStandings || ConstructorStandings);
-            return parseInt(obj[prop]);
+    function getTotalOf(prop) {
+        return Standings.map(({DriverStandings, ConstructorStandings}) => {
+            const [standing] = (DriverStandings || ConstructorStandings);
+            return parseInt(standing[prop]);
         }).reduce((total, num) => total + num, 0);
     }
 
-    function getPodiumsCount() {
-        return Races.filter(race => {
-            const {Results: [{position}]} = race;
-            return parseInt(position) <= 3;
-        }).length;
+    function getPodiums() {
+        return Races.filter(({Results: [{position}]}) => parseInt(position) <= 3).length;
     }
 
-    function getFastestLapsCount() {
+    function getFastestLaps() {
         return Races.filter(race => {
             const {Results: [{FastestLap: {rank} = {}}]} = race;
             return parseInt(rank) === 1;
         }).length;
     }
 
+    function getFinishes(top10 = false) {
+        return Races.filter(({Results: [{positionText}]}) => {
+            const position = parseInt(positionText);
+            return top10 ? position <= 10 : position > 0;
+        }).length;
+    }
+
     return (
         <Card title="Records">
             {loadingStandings && loadingRaces ? <Spinner text="Loading records..."/> : (() => {
-                const rows = [
-                    ['Championships', getTitlesCount()],
-                    ['Entries', Races.length],
-                    ['Pole positions', getPolePositionsCount()],
-                    ['Wins', getTotalCountOf('wins')],
-                    ['Podiums', getPodiumsCount()],
-                    ['Points', getTotalCountOf('points')],
-                    ['Fastest Laps', getFastestLapsCount()]
-                ];
+                const
+                    entries = Races.length,
+                    finishes = getFinishes(),
+                    top10 = getFinishes(true),
+                    wins = getTotalOf('wins');
 
                 return (
                     <>
-                        {rows.map(([label, qty]) =>
-                            <div
-                                key={label}
-                                data-uk-grid=""
-                                className="uk-grid-small">
-                                <div className="uk-width-expand" data-uk-leader="">
-                                    {label}
-                                </div>
-                                <div className="uk-text-bold">{qty}</div>
-                            </div>
-                        )}
+                        <Leader label="Championships" primary={getTitles()}/>
+                        <Leader label="Entries" primary={entries}/>
+                        <Leader
+                            label="Finishes overall"
+                            primary={finishes}
+                            /*secondary={`${round(finishes * 100 / entries)}%`}*/
+                        />
+                        <Leader
+                            label="Finishes in Top 10"
+                            primary={top10}
+                            /*secondary={`${round(top10 * 100 / entries)}%`}*/
+                        />
+                        <Leader
+                            label="Wins"
+                            primary={wins}
+                            /*secondary={`${round(wins * 100 / entries)}%`}*/
+                        />
+                        <Leader label="Pole positions" primary={getPoles()}/>
+                        <Leader label="Podiums" primary={getPodiums()}/>
+                        <Leader label="Points" primary={getTotalOf('points')}/>
+                        <Leader label="Fastest laps" primary={getFastestLaps()}/>
 
                         <div className="uk-text-muted uk-margin-top">
                             <FontAwesomeIcon icon={faInfoCircle}/> Some items may be inaccurate.
